18f88e38fef4d8208232ff4b772c63a07caa5b24
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-05-17 17:34:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import render_template
 from dataparser import DataParser
 from managejson import ManageJson
 from random import randint
-from form_format import Event, Commentaire
+from form_format import Event, Commentaire, User
 import os
 
 
@@ -25,6 +25,7 @@ def get_event(filter: str = "all") -> list:
 app = Flask(__name__)
 app.secret_key = 'azerty123456789'
 
+
 @app.route("/")
 def index():
     events: list = get_event()
@@ -36,24 +37,23 @@ def index():
 
 @app.route("/actualite", methods=["GET", "POST"])
 def actualite():
-    if request.method == "POST":
+    form = Event()
+    if form.validate_on_submit():
         managejson: ManageJson = ManageJson(f"{os.getcwd()}/static/data.json")
-            
         managejson.append(commentaire={
             "type": "actualite",
-            "date": request.form['date'],
-            "titre": request.form['titre'],
-            "description": request.form['desc'],
+            "date": form.date.data.strftime('%m/%d/%y'),
+            "titre": form.titre.data,
+            "description": form.description.data,
             "id": randint(50,50000)
         })
-
         return redirect(url_for('actualite'))
 
     events: list = get_event("actualite")
     commentaires: list = get_comment()
-    form = Commentaire()
+    commentaire_form = Commentaire()
     islogin: bool = True if len(session) > 0 else False  
-    return render_template("index.html", events=events, commentaires=commentaires, login=islogin, form=form)
+    return render_template("index.html", events=events, commentaires=commentaires, login=islogin, form=commentaire_form)
 
 
 @app.route("/add_concerts")
@@ -61,8 +61,9 @@ def add_concerts():
     if not session.get("user", "") and not session.get("password", ""):
         return redirect(url_for('login'))
 
+    form = Event()
     islogin: bool = True if len(session) > 0 else False
-    return render_template("add_concerts.html", login=islogin)
+    return render_template("add_concerts.html", login=islogin, form=form)
 
 
 @app.route("/add_actualite")
@@ -70,34 +71,34 @@ def add_actualite():
     if not session.get("user", "") and not session.get("password", ""):
         return redirect(url_for('login'))
 
+    form = Event()
     islogin: bool = True if len(session) > 0 else False
-    return render_template("add_actualite.html", login=islogin)
+    return render_template("add_actualite.html", login=islogin, form=form)
 
 
 @app.route("/concerts", methods=["GET", "POST"])
 def concerts():
-    if request.method == "POST":
+    form = Event()
+    if form.validate_on_submit():
         managejson: ManageJson = ManageJson(f"{os.getcwd()}/static/data.json")
-            
         managejson.append(commentaire={
             "type": "Concerts",
-            "date": request.form['date'],
-            "titre": request.form['titre'],
-            "description": request.form['desc'],
+            "date": form.date.data.strftime('%m/%d/%y'),
+            "titre": form.titre.data,
+            "description": form.description.data,
             "id": randint(50,50000)
         })
-
         return redirect(url_for('concerts'))
 
     events: list = get_event("Concerts")
     commentaires: list = get_comment()
-    form = Commentaire()
+    commentaire_form = Commentaire()
     islogin: bool = True if len(session) > 0 else False 
-    return render_template("concerts.html", events=events, commentaires=commentaires, login=islogin, form=form)
+    return render_template("concerts.html", events=events, commentaires=commentaires, login=islogin, form=commentaire_form)
 
 
 @app.route("/rock")
-def Rock():
+def rock():
     events: list = get_event("Rock")
     commentaires: list = get_comment()
     form = Commentaire()
@@ -113,6 +114,7 @@ def jazz():
     islogin: bool = True if len(session) > 0 else False 
     return render_template("jazz.html", events=events, commentaires=commentaires, login=islogin, form=form)
 
+
 @app.route("/electro")
 def electro():
     events: list = get_event("Electro")
@@ -124,30 +126,31 @@ def electro():
 
 @app.route("/commentaire", methods=["GET", "POST"])
 def commentaire():
-    if request.method == "POST":
+    form = Commentaire()
+    if form.validate_on_submit():
         managejson: ManageJson = ManageJson(f"{os.getcwd()}/static/commentaire.json")
-            
         managejson.append(commentaire={
-            "nom": request.form['nom'],
-            "commentaire": request.form['commentaire']
+            "nom": form.nom.data,
+            "commentaire": form.commentaire.data
         })
-
         return redirect(url_for('actualite'))
 
     return redirect(url_for('actualite'))
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
+    form = User()
+    if form.validate_on_submit():
         if session.get("user", "NA") == "NA":
-            session["user"] = request.form['user']
-            session["password"] = request.form['password']
+            session["user"] = form.user.data
+            session["password"] = form.password.data
             return redirect(url_for('actualite'))
         else:
             return redirect(url_for('actualite'))
     else:
-        islogin: bool = True if len(session) > 0 else False 
-        return render_template("login.html", login=islogin)
+        islogin: bool = True if len(session) > 0 else False
+        return render_template("login.html", login=islogin, form=form)
 
 
 @app.route("/logout", methods=["GET"])
@@ -158,6 +161,7 @@ def logout():
     session.pop("user")
     session.pop("password")
     return redirect(url_for('actualite'))
+
 
 @app.route("/delete", methods=["GET"])
 def delete_event():
@@ -170,7 +174,7 @@ def delete_event():
     managejson: ManageJson = ManageJson(f"{os.getcwd()}/static/data.json")
     managejson.delete_ligne(int(request.args.get('id')))
     return redirect(url_for('actualite'))
-    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
